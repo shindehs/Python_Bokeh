ffa6dd89e2e4fd3474a6603b01e86f5982ea1dd0
hanmant.shinde <hanmant.shinde@harbingergroup.com>
2019-09-06 12:26:33 +0530

    second Commit

---
+++ 01_Python_Bokeh_Plot_Circle.py
@@ -0,0 +1,21 @@
+# Making a basic Bokeh Graph
+
+# importing Bokeh
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+
+# prepare some data
+x =[1,2,3,4,5]
+y =[6,7,8,9,10]
+
+# prepare the output file
+output_file("Circle_html")
+
+# create figure file
+f = figure()
+
+# create line plot
+f.circle(x,y)
+
+#write the plot in figure object
+show(f)
---
+++ 02_Python_Bokeh_Pandas_Csv.py
@@ -0,0 +1,8 @@
+# Importing data from pandas and Csv and plotting using bokeh
+
+# Importing Bokeh and Pandas
+
+from bokeh.plotting import figure
+from bokeh.io import output_file, show
+
+# prepare some data , import from csv
